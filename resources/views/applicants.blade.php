030f98b0e23a7aa6abb7b7505b5e599d77266b31
manuelvidales <39100073+manuelvidales@users.noreply.github.com>
2019-10-16 09:46:49 -0500

    corregion input upload & layout

--- resources/views/applicants.blade.php
+++ resources/views/applicants.blade.php
@@ -1,31 +1,5 @@
 @extends('layouts.custom')
-@if (session('info'))
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="alert alert-success" role="alert">
-                    <strong>Aviso:</strong> {{ session('info') }}
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-            </div>
-        </div>
-    </div>
-@elseif (session('error'))
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="alert alert-danger" role="alert">
-                    <strong>Aviso:</strong> {{ session('error') }}
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-            </div>
-        </div>
-    </div>
-@endif
+@section('content')
 <div class="container">
 	<div class="row justify-content-center">
 		<div class="col-md-8">
@@ -51,27 +25,34 @@
 			<input type="text" name="phone" class="form-control" placeholder="Telefono de casa" value="{{old('phone')}}">
 			<label style="color:red">{!! $errors->first('phone', '<small>:message</small><br>') !!}</label>
 			</div>
-
 			<div class="input-group mb-3">
-			  <div class="input-group-prepend">
+				<div class="input-group-prepend">
 			    <span class="input-group-text" id="inputGroupFileAddon01">Subir</span>
-			  </div>
-			  <div class="custom-file">
-			    <input type="file" name="resume" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
-			    <label class="custom-file-label" for="inputGroupFile01">Seleccionar archivo</label>
-			  </div>
-			  <label style="color:red">{!! $errors->first('resume', '<small>:message</small><br>') !!}</label>
+			  	</div>
+			  	<div class="custom-file">
+			     <input type="file" name="resume" class="custom-file-input" id="customFile">
+			     <label class="custom-file-label" for="customFile">Ningun archivo selecionado</label>
+				 </div>
 			</div>
-
-			<br>
+			<div>
+			<label style="color:red">{!! $errors->first('resume', '<small>:message</small><br>') !!}</label>
+		</div>
 			<div class="justify-content-end">
-
-				<button type="submit" class="btn btn-success btn-lg btn-block"> <strong>Enviar</strong> </button>
+			<button type="submit" class="btn btn-success btn-lg btn-block"> <strong>Enviar</strong> </button>
 			</div>
 		</form>
 
+<script>
+// codigo para que aparezca el nombre del archivo selecionado
+$(".custom-file-input").on("change", function() {
+  var fileName = $(this).val().split("\\").pop();
+  $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+});
+</script>
+
 </div>
 </div>
 </div>
 </div>
 </div>
+@endsection
--- resources/views/layouts/custom.blade.php
+++ resources/views/layouts/custom.blade.php
@@ -7,15 +7,47 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Contacto') }}</title>
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>    
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     <!-- Styles -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">    
 </head>
 <body>
-
+    <div id="app">
+@if (session('info'))
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="alert alert-success" role="alert">
+                    <strong>Aviso:</strong> {{ session('info') }}
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+@elseif (session('error'))
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="alert alert-danger" role="alert">
+                    <strong>Aviso:</strong> {{ session('error') }}
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+@endif
+        <main class="py-4">
+        @yield('content')
+        </main>
+    </div>
 </body>
 
 </html>
